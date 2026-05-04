7965199259650810d952831127f24eebe26b9c8b
Uday2k5 <uday1872005@gmail.com>
2026-05-04 13:01:27 +0530

    Allow deployed frontend origin

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","aapda-ai-disaster-management-projec.vercel.app",],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://aapda-ai-disaster-management-projec.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
